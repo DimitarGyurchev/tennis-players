e493d8bbef631b4b317b93e6c24c6f3218c5b563
Dimitar Gyurchev <dimitargyurchev93@gmail.com>
2023-03-22 15:15:47 +0100

    First Init

---
+++ src/main/java/com/softuni/tennis_players/TennisPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.tennis_players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisPlayersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softuni/tennis_players/TennisPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.tennis_players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
