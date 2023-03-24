c476fd200eccf4062c13d7ca06ba75fa703af1d3
Dimitar Gyurchev <dimitargyurchev93@gmail.com>
2023-03-24 18:21:29 +0100

    added new fragments

---
+++ src/main/java/com/softuni/tennis_players/web/RegistrationController.java
@@ -0,0 +1,54 @@
+package com.softuni.tennis_players.web;
+
+import com.softuni.tennis_players.domain.dtos.binding.UserRegisterFormDto;
+import com.softuni.tennis_players.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/users")
+public class RegistrationController {
+
+    private static final String BINDING_RESULT_PATH = "org.springframework.validation.BindingResult.";
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(
+            @Valid UserRegisterFormDto userRegisterForm,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes
+                    .addFlashAttribute("userRegisterForm", userRegisterForm)
+                    .addFlashAttribute(BINDING_RESULT_PATH + "userRegisterForm", bindingResult);
+
+            return "redirect:/users/register";
+        }
+
+        userService.registerUser(userRegisterForm);
+
+        return "redirect:/users/login";
+    }
+
+    @ModelAttribute(name = "userRegisterForm")
+    public UserRegisterFormDto initUserRegisterFormDto() {
+        return new UserRegisterFormDto();
+    }
+
+}
